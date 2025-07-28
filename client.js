83b14d972d912a78e1f6906007c637b2fefbf139
Wojtek22k <michalskiwojtek771@gmail.com>
2025-07-28 11:40:53 +0200

    Update client.js

--- client.js
+++ client.js
@@ -6,27 +6,7 @@ window.TrelloPowerUp.initialize({
         return t.popup({
           title: 'Dodaj zlecenie',
           url: 'index.html',
-          height: 150
-        });
-      }
-    }];
-  },
-  'card-buttons': function (t) {
-    return [{
-      text: 'Weź zlecenie',
-      callback: function (t) {
-        return t.member('id').then(function (member) {
-          return t.get('card', 'shared', 'claimedBy').then(function (claimedBy) {
-            if (claimedBy === member.id) {
-              alert('Już masz to zlecenie.');
-            } else if (claimedBy) {
-              alert('To zlecenie jest już zajęte.');
-            } else {
-              return t.set('card', 'shared', 'claimedBy', member.id).then(function () {
-                alert('Zlecenie przypisane!');
-              });
-            }
-          });
+          height: 300
         });
       }
     }];
