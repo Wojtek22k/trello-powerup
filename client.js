9b504b9066068e95a49d33613731121dd8df30f3
Wojtek22k <michalskiwojtek771@gmail.com>
2025-07-28 11:26:04 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,31 +1,29 @@
 window.TrelloPowerUp.initialize({
-  'board-buttons': function(t) {
+  'board-buttons': function (t) {
     return [{
       text: 'Nowe zlecenie',
-      callback: function(t) {
+      callback: function (t) {
         return t.popup({
-          title: 'Dodaj nowe zlecenie',
-          url: './index.html',
+          title: 'Dodaj zlecenie',
+          url: 'index.html',
           height: 150
         });
       }
     }];
   },
-  'card-buttons': function(t) {
+  'card-buttons': function (t) {
     return [{
       text: 'Weź zlecenie',
-      callback: function(t) {
-        return t.member('id').then(function(member) {
-          return t.card('id', 'idMembers').then(function(card) {
-            if(card.idMembers.includes(member.id)) {
+      callback: function (t) {
+        return t.member('id').then(function (member) {
+          return t.get('card', 'shared', 'claimedBy').then(function (claimedBy) {
+            if (claimedBy === member.id) {
               alert('Już masz to zlecenie.');
+            } else if (claimedBy) {
+              alert('To zlecenie jest już zajęte.');
             } else {
-              return t.card('id').then(function() {
-                return t.member('id').then(function(member) {
-                  return t.set('card', 'shared', 'claimedBy', member.id)
-                    .then(() => t.attach({ url: `https://trello.com/member/${member.id}` }))
-                    .then(() => alert('Wziąłeś zlecenie!'));
-                });
+              return t.set('card', 'shared', 'claimedBy', member.id).then(function () {
+                alert('Zlecenie przypisane!');
               });
             }
           });
