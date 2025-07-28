fd3eb54174f1c8c6035cbd436b66727530185aed
Wojtek22k <michalskiwojtek771@gmail.com>
2025-07-28 11:45:22 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,14 +1,16 @@
+function showNewTaskForm(t) {
+  return t.popup({
+    title: 'Dodaj zlecenie',
+    url: 'index.html',
+    height: 300
+  });
+}
+
 window.TrelloPowerUp.initialize({
   'board-buttons': function (t) {
     return [{
       text: 'Nowe zlecenie',
-      callback: function (t) {
-        return t.popup({
-          title: 'Dodaj zlecenie',
-          url: 'index.html',
-          height: 300
-        });
-      }
+      callback: showNewTaskForm
     }];
   }
 });
